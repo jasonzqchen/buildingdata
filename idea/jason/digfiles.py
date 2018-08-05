1d8cadb7d2404a73f3296b24b2c2e67a51f1dffc
Jason <jasonzqchen@gmail.com>
2018-08-05 23:27:26 +0800

    digfiles updated
    using class to better manage the functions

--- idea/jason/digfiles.py
+++ idea/jason/digfiles.py
@@ -12,205 +12,262 @@ import re
 import time 
 import shutil
 
-os.getcwd()
-
-
-# %%   
-def _subdirs(path):
-    """Yield directory names not starting with '.' under given path."""
-    try:
-        for entry in os.scandir(path):
-            if not entry.name.startswith('.') and entry.is_dir():
-                yield entry
-    except PermissionError as reason:
-            pass #print('No permission'+ str(reason))
-    except:
-            pass #print('Error..')
-            
-def _subfiles(path):
-    """Yield directory names not starting with '.' under given path."""
-    try:
-        for entry in os.scandir(path):
-            if not entry.name.startswith('.') and entry.is_file():
-                yield entry
-    except PermissionError as reason:
-            pass #print('No permission'+ str(reason))
-    except:
-            pass #print('Error..')
- 
-def _createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print ('Error: Creating directory. ' +  directory)
-
-def _copyfile(store, savepath, overwrite = 0):
-    try:
-        for each in store:
-            #print('each: ', each)
-            directory = savepath + os.sep + each.split('\\')[-1]
-            if not os.path.exists(directory):
-                shutil.copy2(each, savepath)
-            elif os.path.exists(directory):
-                if overwrite == 0:
-                    temp = '(1)' + each.split('\\')[-1]
-                    modifiedpath = savepath + os.sep + temp
-                    shutil.copy2(each, modifiedpath)
-                else:
-                    shutil.copy2(each, savepath) ## can this overwrite??
-    except OSError:
-        print ('Error: Creating directory. ' +  savepath)
+# %% 
+class Digfiles:
+    def __init__(self, paths, savepath):    
+        self.paths = paths # main search master path
+        self.savepath = savepath # save to which path
+        self.folderkey = [] # the keywords for searching prioritised folders
+        self.filekey = [] # the keywords for searching prioritised files
+        self.datadict = dict() # dictionary to store all the files for all projects
+    
+    def runsetting(self, save=0, overwrite=0, semiwalk= 0, fullwalk=0):
+        '''
+        ******run settings*******
+        save: save the found files to the target folder
+        overwrite: save the found files to the target folder and overwrite 
+                    if there's same files
+        semiwalk: semiwalk is in between keywords and full os.walk method, it use os.walk
+                    when cannot use keywords to find the file
+        fullwalk: call os.walk functions (slowfindfile) for full path search, very slow..
         
-def _loop(path, store):
-    for eachEntry in _subdirs(path):
-        if re.search(folderkey, eachEntry.name, re.IGNORECASE): #search the shallow layer
-            _loop(eachEntry.path, store) # search the deeper layer
-            #print('EPR: ', eachEntry.path)
-            for eachFile in _subfiles(eachEntry.path):
-                if not eachFile.name.startswith('~') and \
-                re.search(filekey, eachFile.name, re.IGNORECASE): # this is to find the files, to be modified
-                    store.append(eachFile.path)
-    for eachFile in _subfiles(path):
-            if not eachFile.name.startswith('~') and \
-            re.search(filekey, eachFile.name, re.IGNORECASE): # this is to find the files, to be modified
-                store.append(eachFile.path)   
-    return store
+        '''
+        self.save = save
+        self.overwrite = overwrite
+        self.semiwalk = semiwalk
+        self.fullwalk = fullwalk
+    
+    def setfolderkey(self, key):
+        self.folderkey = re.compile(key, re.IGNORECASE)
 
-def _loopfolders(paths):
-    folders = []
-    for path in paths:
-        for eachEntry in _subdirs(path):
-            if eachEntry.is_dir():
-                folders.append(eachEntry.path)
-    return folders
-
-def _loopfoldersName(paths):
-    names = []
-    for path in paths:
-        for eachEntry in _subdirs(path):
-            if eachEntry.is_dir():
-                names.append(eachEntry.name)
-    return names
-
-def _findfolders(folders):
-    newpaths = []
-    for each in folders:
-        if re.search(folderkey, each, re.IGNORECASE):
-            newpaths.append(each)
-    return newpaths
-
-def _looplooploop(newpaths):
-    listoffolders = _loopfolders(newpaths)
-    newpaths = _findfolders(listoffolders)
-    #print(newpaths)
-    return listoffolders, newpaths
+    def setfilekey(self, key):
+        self.filekey = re.compile(key, re.IGNORECASE)
         
-def _slowfindfile(filekey, path):
-    result = []
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if re.search(filekey,file,re.IGNORECASE) is not None:
-                result.append(os.path.join(root, file))
-    return result
-        
-def _main(path, full):
-    store = []
-    newpaths = []
-    while True:
-        if store != []: 
-            # if there's folderkey found in the layer
-            # this speeds up the searching logic a lot, however, may have chance to miss
-            # the file if the wanted files are not put into the folder searched by
-            # folderkey.
-            store = _loop(path, store) # step 1
-            break
-        else:
-            # if there no folderkey in the layer
-            newpaths = []
-            newpaths.append(path)
-            counter = 0
-            while True:
-                # here is to loop to search the folder based on keywords
-                (listoffolders, newpaths) = _looplooploop(newpaths)
-                if newpaths == []:
-                    (listoffolders, newpaths) = _looplooploop(listoffolders)
-                else:
-                    (listoffolders, newpaths) = _looplooploop(newpaths)
-                    
-                counter += 1
-                if counter > 100:  #ok...if cannot search through finding folderkey...then let's do os.walk..
-                                    # through all files....
-                    if full:
-                        store = _slowfindfile(filekey, path)
-                    return store
-                 
-                # after we find the paths for folders..let's continue
-                if len(newpaths) == 1:
-                    store = _loop(newpaths[0], store)
-                    return store
-                if len(newpaths) > 1:
-                    for newpath in newpaths:
-                        #print(newpath)
-                        store = _loop(newpath, store)
-                    return store
+    def subdirs(self, path):
+        """Yield directory names not starting with '.' under given path."""
+        try:
+            for entry in os.scandir(path):
+                if not entry.name.startswith('.') and entry.is_dir():
+                    yield entry
+        except PermissionError as reason:
+                pass #print('No permission'+ str(reason))
+        except:
+                pass #print('Error..')
                 
-def _input(path, name, datadict, savepath = [], full=0, save=0, overwrite=0):                
-    #start = time.time()
-    global folderkey, filekey
-    folderkey = '(超限)|(抗震)|(EPR)|(报告)|(Report(s)?)'
-    #folderkey = u'(Report(s)?)|(Calc)|(楼面)|(抗震)|(EPR)'
-    filekey = '(超限)|(抗震)|(EPR)'
-    #filekey = u'(楼面体系)|(钢筋桁架)|(组合楼板)'
+    def subfiles(self, path):
+        """Yield directory names not starting with '.' under given path."""
+        try:
+            for entry in os.scandir(path):
+                if not entry.name.startswith('.') and entry.is_file():
+                    yield entry
+        except PermissionError as reason:
+                pass #print('No permission'+ str(reason))
+        except:
+                pass #print('Error..')
+#----------------------------------------------------------------------------      
+    # this is a recursive function to update path and store all the files found            
+    def loop(self, path, store):
+        entries = Digfiles.subdirs(self, path)
+        for eachEntry in entries:
+            if self.folderkey.search(eachEntry.name): #search the shallow layer
+                Digfiles.loop(self, eachEntry.path, store) # search the deeper layer
+                
+        files = Digfiles.subfiles(self, path)
+        for eachFile in files:
+                if not eachFile.name.startswith('~') and \
+                self.filekey.search(eachFile.name): # this is to find the files, to be modified
+                    store.append(eachFile.path)   
+        return store
     
-    # 1. 用_loop找path，找到文件夹关键字（如folder）就继续搜索一直搜到有文件名关键字为止
-    # 2. 如果step 1找不到return[]，要深度搜索直至搜到文件夹关键字为止，跳回step 1
-    store = _main(path, full)
-    #print('\n' + name)
-    #print(store)
-    if store == []:
-        pass #print('Warning: cannot find the files, please review the targeted folder or filekey')
-    else:
-        datadict[name] = [store]
+#----------------------------------------------------------------------------  
+    def loopfoldersName(self, paths):
+        names = []
+        for path in paths:
+            entries = Digfiles.subdirs(self, path)
+            for eachEntry in entries:
+                names.append(eachEntry.name)
+        return names
     
-    #print('%.2f %s' % ((time.time()-start),'s')) 
-    #start = time.time()
-    if save and store !=[]:
-        _createFolder(savepath + os.sep + name)
-        savepathfull = savepath + os.sep + name
-        print('copying...', name)
-        _copyfile(store, savepathfull, overwrite)
-    #print('%.2f %s' % ((time.time()-start),'s'))
-# %%  
-if __name__ == '__main__':
+    def loopfolders(self, paths):
+        for path in paths:
+            folders = [eachEntry.path for eachEntry in Digfiles.subdirs(self, path)]
+            files = [eachFile.path for eachFile in Digfiles.subfiles(self, path)]
+        return folders, files
+
+    def findfolders(self, folders, files):       
+        ffolders = [folder for folder in folders if self.folderkey.search(folder)]
+        ffiles = [file for file in files if self.filekey.search(file)]
+        return ffolders, ffiles
+    
+    def ziploop(self, newpaths):
+        (listoffolders, listoffiles) = Digfiles.loopfolders(self, newpaths)
+        (newpaths, ffiles) = Digfiles.findfolders(self, listoffolders, listoffiles)
+        #print(newpaths)
+        return listoffolders, listoffiles, newpaths, ffiles
+#----------------------------------------------------------------------------
+    # others 
+    def createFolder(self, directory):
+        '''
+        this function can be used to create folders
+        '''
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+        except OSError:
+            print ('Error: Creating directory. ' +  directory)
+    
+    def copyfile(self, files, saveto):
+        '''
+        this function can be used to copy files
+        files: a list of file paths
+        saveto: a path that all files will be copied to
+        '''
+        try:
+            for each in files:
+                #print('each: ', each)
+                fullpath = saveto + os.sep + each.split('\\')[-1]
+                if not os.path.exists(fullpath):
+                    shutil.copy2(each, saveto)
+                elif os.path.exists(fullpath):
+                    if self.overwrite == 0:
+                        temp = '(1)' + each.split('\\')[-1]
+                        modifiedpath = saveto + os.sep + temp
+                        shutil.copy2(each, modifiedpath)
+                    else:
+                        shutil.copy2(each, saveto) ## can this overwrite??
+        except OSError:
+            print ('Error: Creating directory. ' +  savepath)     
+            
+    def slowfindfile(self, path):
+        '''
+        this function use os.walk to walk through all directories in path, and
+        return with a stored function storing all the file path that meet the
+        filekey search
+        
+        use with cautions -> very slow
+        '''
+        store = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if self.filekey.search(file) is not None:
+                    store.append(os.path.join(root, file))
+        return store
+    
+#----------------------------main execution code---------------------------------
+
+    def main(self, path):
+        '''
+        main function to define logic to find the targeted folders and files
+        Basic logic:
+        Three senerios:
+        1. if there's folderkey found in the layer.
+            this speeds up the searching logic a lot; however, may have chance to miss
+            the file if the wanted files are not put into the folder searched by
+            folderkey.
+        2. if there no folderkey in the layer, then go to deeper layer to find
+        3. semi-walking is in between keywords and full os.walk method, it use os.walk
+            when cannot use keywords to find the file
+        4. call os.walk functions (slowfindfile) for full path search, very slow..
+                
+        '''
+        store = []
+        
+        while True:
+            #print('method 1...shallow.digging...', path)
+            store = Digfiles.loop(self, path, store) # step 1
+            if self.fullwalk:
+                #print('method 4...fullwalking....', path)
+                store = Digfiles.slowfindfile(self, path)
+                return store
+            elif store != []: 
+                # if there's folderkey found in the layer
+                # this speeds up the searching logic a lot, however, may have chance to miss
+                # the file if the wanted files are not put into the folder searched by
+                # folderkey.
+                return store
+            else:
+                #print('method 2...deep.digging..', path)
+                # if there no folderkey in the layer, go deeper
+                newpaths = []
+                newpaths.append(path)
+                counter = 0
+                
+                (listoffolders, lfis, newpaths, ffis) = Digfiles.ziploop(self, newpaths)
+                
+                while True:
+                    # here is to loop to search the folder based on keywords
+                    if newpaths == []: 
+                        # if the first search didn't get the anything, then we need to
+                        # feed back the all the folderpath back to loop and find the
+                        # next layer of folders
+                        if listoffolders != []:
+                            (listoffolders, lfis, newpaths, ffis) = Digfiles.ziploop(self, listoffolders)
+                            if ffis != []:
+                                for ffi in ffis:
+                                    store.append(ffi)
+                    if len(newpaths) == 1:
+                        store = Digfiles.loop(self, newpaths[0], store)
+                        return store
+                    if len(newpaths) > 1:
+                        for newpath in newpaths:
+                            #print(newpath)
+                            store = Digfiles.loop(self, newpath, store)
+                        return store
+                    
+                    counter += 1
+                    if counter > 100:  #ok...if cannot search through finding folderkey...then let's do os.walk..
+                                        # through all files....
+                        if self.semiwalk:
+                            #print('method 3...deep.digging..', path)
+                            store = Digfiles.slowfindfile(self, path)
+                            #print('\n',path,'\n')
+                            #print(store)
+                        return store
+                
+    def store2dict(self, path, name):             
+        '''
+        store the data into dictionary with each main folder name
+        and copy to the target folder
+        '''
+        store = Digfiles.main(self, path)
+        #print('\n' + name)
+        #print(store)
+        if store == []:
+            pass #print('!Warning: cannot find the files, please review the targeted folder or filekey')
+        else:
+            self.datadict[name] = [store]
+        
+        if self.save and store !=[]:
+            Digfiles.createFolder(self, self.savepath + os.sep + name)
+            savepathfull = self.savepath + os.sep + name
+            print('copying...', name)
+            Digfiles.copyfile(self, store, savepathfull)
+    
+    def run(self):
+        (projectpaths,files) = df.loopfolders(self.paths)
+        projectnames = df.loopfoldersName(self.paths)
+        for projectpath, projectname in zip(projectpaths, projectnames):
+            Digfiles.store2dict(self, projectpath, projectname)
+
+        
+if __name__ == '__main__':        
     start = time.time()
     
-    paths = [r'G:',r'N:\STR-SHANTS02\项目',r'N:\STR-SHANTS03']
-    #paths = [r'G:\251242 - CRC SHA SHW']
-    savepath = r'C:\Users\jason.chen\Documents\JASON\EPR'
+    paths = paths = [r'C:\Users\Ziqiang\OneDrive - Arup']
+    savepath = r'C:\Users\Ziqiang\Desktop\EPR'
+    df = Digfiles(paths, savepath)
     
-    datadict = {}
-    projectpaths = _loopfolders(paths)
-    projectnames = _loopfoldersName(paths)
-    for projectpath, projectname in zip(projectpaths, projectnames):
-        _input(projectpath, projectname, datadict, savepath, 1, 1, 0)
-
+    folderkeyword = '(超限)|(抗震)|(EPR)|(报告)|(Report(s)?)'
+    filekeyword = '(超限)|(抗震)|(EPR)'
+    df.setfolderkey(folderkeyword)
+    df.setfilekey(filekeyword)    
+    df.runsetting(0,0,1,0)
+    # save
+    # overwrite
+    # semiwalk
+    # fullwalk
+    
+    df.run()
+    filedict = df.datadict
+    
     print(time.time() - start)
-
-# %% 从头到尾深度搜索
-start = time.time()    
-
-#filekey = u'(楼面体系)|(钢筋桁架)|(组合楼板)'
-#paths = [r'G:', r'N:\STR-SHANTS02\项目', r'N:\STR-SHANTS03']
-folderkey = '(超限)|(抗震)|(EPR)|(报告)|(Report(s)?)'
-filekey = '(超限)|(抗震)|(EPR)'
-datadict = {}
-projectpaths = _loopfolders(paths)
-projectnames = _loopfoldersName(paths)
-
-for projectpath, projectname in zip(projectpaths, projectnames):
-    result = _slowfindfile(filekey, projectpath)
-    if result != []:
-        datadict[projectname] = result
-    
-print(time.time() - start)
